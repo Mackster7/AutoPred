e11835ff8b408a6179724391ec84366f2ab5dd3d
Mackster7 <imaco.mansoor@gmail.com>
2021-12-25 16:57:49 +0530

    preauto second commit

--- app/src/main/java/com/example/autoconsultancy/MainActivity2.java
+++ app/src/main/java/com/example/autoconsultancy/MainActivity2.java
@@ -7,11 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity2 extends AppCompatActivity {
     public TextView price,price_plus;
-    public Button ok_button;
+    public Button ok_button,olx;
+    public ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +26,62 @@ public class MainActivity2 extends AppCompatActivity {
         price=(TextView) findViewById(R.id.price);
         price_plus=(TextView) findViewById(R.id.priceplus);
         ok_button=(Button) findViewById(R.id.close_button);
+        image=(ImageView) findViewById(R.id.logo);
+        olx=(Button) findViewById(R.id.olx);
 
         String value= getIntent().getStringExtra("key");
         System.out.println("val is "+value);
-        price.setText(value);
 
         int value1=Integer.parseInt(value);
-        value1=value1+((value1/100)*5);
+        int value2=value1+((value1/100)*5);
         System.out.println("val1 is "+value1);
-        String value_plus=String.valueOf(value1);
-        price_plus.setText(value_plus);
+
+
+        //converting to indian format...
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance(new Locale("en", "in"));
+        String myCurrency = currency.format(value1);
+
+        price.setText(myCurrency);
+        NumberFormat currency_plus = NumberFormat.getCurrencyInstance(new Locale("en", "in"));
+        String myCurrency_plus = currency.format(value2);
+
+        price_plus.setText(myCurrency_plus);
+
 
         ok_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(in);
+                finish();
+            }
+        });
+
+        olx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.olxgroup.panamera.app.users.onboarding.activities.SplashActivity");
+
+                if (launchIntent != null) {
+                    startActivity(launchIntent);//null pointer check in case package name was not found
+                }
+                else
+                {
+                    Toast.makeText(MainActivity2.this,"there is no such package",Toast.LENGTH_LONG).show();
+                }*/
+
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setClassName("com.olx.southasia", "com.olxgroup.panamera.app.users.onboarding.activities.SplashActivity");
+                startActivity(intent);
             }
         });
     }
